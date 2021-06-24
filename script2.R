719cb5767e3e5178c81682bdebd7c30ea4c9a537
auvy <datruegvoltage@gmail.com>
2021-06-24 19:10:08 +0300

    Delete script2.R

--- script2.R
+++
@@ -1,7 +0,0 @@
-#install.packages("dplyr")
-#install.packages("ggplot2")
-#library(dplyr)
-#library(ggplot2)
-#p <- ggplot(flats, aes(x=Загальна_площа)) + geom_bar(fill="lightblue", col="grey") + ylab('Кількість')
-#p
-ggplot(flats, aes(x=Загальна_площа, y=Ціна)) + geom_point()
